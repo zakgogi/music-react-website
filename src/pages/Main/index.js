d9ffc7df91d08098a08bf1da1525bf2d22d1e8e0
Zak Gogi <zakgogi1@gmail.com>
2021-08-04 10:16:30 +0100

    added border styling for activity section

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -12,7 +12,7 @@ function Main() {
         <h1>Favourite Artist</h1>
         <Artist />
         <Albums />
-        <section>
+        <section id="activitySection">
           <h3>Why not try an activity while you listen to some of this music</h3>
           { showActivity ? <Activity close={toggleActivity} /> : <button id="findActivity" onClick={toggleActivity}>Find Activities</button>}
         </section>
